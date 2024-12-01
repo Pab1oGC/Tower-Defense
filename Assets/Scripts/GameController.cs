80a7aa3e5e8b11acb68bfde4a8b97ee395c9dcf9
Pablo Gutierrez P <pablogutierrrez0@gmail.com>
2024-12-01 19:09:41 -0400

    no se rey, yo no subo zips

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,33 +2,146 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameController : MonoBehaviour
 {
     public GameObject panel;
     public Text lblPoints;
+    public Text lblAmountWolf;
+    public Text lblAmountPj1;
+    public Text lblAmountPj3;
     public int points;
+
+    int priceWolf = 25;
+    int pricePj1 = 10;
+    int pricePj3 = 50;
+
+    public int amountWolf = 0;
+    public int amountPj1 = 0;
+    public int amountPj3 = 0;
+
+    public List<Transform> containers = new List<Transform>();
+    public GameObject prefabLevel;
+
+    int[] levelsBought = new int[9];
+
+    public int basePriceLevel = 100;
+    public List<Text> textsPrice = new List<Text>();
+
+    public Texture2D defaultCursor;
+    public static GameController instance;
     // Start is called before the first frame update
     void Start()
     {
         points = 1000;
+        amountWolf = 0;
+        amountPj1 = 0;
+        amountPj3 = 0;
+        if (lblPoints!=null)
+        lblPoints.text = points.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        lblPoints.text = $"Points: {points}";
+        lblAmountPj1.text = amountPj1.ToString();
+        lblAmountWolf.text = amountWolf.ToString();
+        lblAmountPj3.text = amountPj3.ToString();
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
             panel.SetActive(!panel.activeSelf);
+            UnityEngine.Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto);
         }
     }
-    public void BuyNormal()
+
+    void Awake()
     {
-        points--;
+        if (instance == null)
+            instance = this;
     }
-    public void BuyNormal2()
+
+    public void BuyCharacters(int numero)
     {
-        points++;
+        switch (numero)
+        {
+            case 1:
+                VerifyAndUpdate(pricePj1, ref amountPj1);
+                break;
+            case 2:
+                VerifyAndUpdate(priceWolf, ref amountWolf);
+                break;
+            case 3:
+                VerifyAndUpdate(pricePj3, ref amountPj3);
+                break;
+        }
+      
     }
+
+    public void BuyLevel(int index)
+    {
+        //Marcy o Lucho aca aumenta la logica para aumentar las stats de cada personaje, por eso esta el switch
+        switch (index)
+        {
+            case 0:
+                AddLevel(index);
+                break;
+            case 1:
+                AddLevel(index);
+                break;
+            case 2:
+                AddLevel(index);
+                break;
+            case 3:
+                AddLevel(index);
+                break;
+            case 4:
+                AddLevel(index);
+                break;
+            case 5:
+                AddLevel(index);
+                break;
+            case 6:
+                AddLevel(index);
+                break;
+            case 7:
+                AddLevel(index);
+                break;
+            case 8:
+                AddLevel(index);
+                break;
+        }
+    }
+
+    void VerifyAndUpdate(int price,ref int amount)
+    {
+        if (points >= price)
+        {
+            amount++;
+            points -= price;
+            lblPoints.text = points.ToString();
+        }
+    }
+
+    void AddLevel(int index)
+    {
+        int purchases = levelsBought[index];
+        int actualPrice = Mathf.RoundToInt(basePriceLevel * Mathf.Pow(1.2f, purchases));
+        if (containers[index].childCount < 6 && points >= actualPrice)
+        {
+            Instantiate(prefabLevel, containers[index]);
+            points -= actualPrice;
+            levelsBought[index]++;
+            lblPoints.text = points.ToString();
+            textsPrice[index].text = $"Precio: {Mathf.RoundToInt(basePriceLevel * Mathf.Pow(1.2f, levelsBought[index]))}";
+        }
+            
+    }
+
+    public bool CheckInPanel()
+    {
+        return !panel.activeSelf;
+    }
+    
 }
--- Assets/Scripts/RespawnAllies.cs
+++ Assets/Scripts/RespawnAllies.cs
@@ -8,34 +8,58 @@ public class RespawnAllies : MonoBehaviour
     public Texture2D currentCursor;
     public Texture2D defaultCursor;
     public Texture2D putCursor;
+    public static RespawnAllies instance;
+
+    public List<Transform> respawns = new List<Transform>();
+    int currentCharacter;
+
+    public GameObject pj1Prefab;
+    public GameObject wolfPrefab;
+    public GameObject pj3Prefab;
     void Start()
     {
         SetCursor(defaultCursor);
+        currentCharacter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Alpha1)) currentCharacter = 1;
+        if(Input.GetKeyDown(KeyCode.Alpha2)) currentCharacter = 2;
+        if(Input.GetKeyDown(KeyCode.Alpha3)) currentCharacter = 3;
     }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
     void OnMouseDown()
     {
-        switch (lane)
+        if (GameController.instance.CheckInPanel())
         {
-            case Lane.Top:
-                Debug.Log($"Se hizo clic en la zona: TOP");
-                break;
-            case Lane.Right:
-                Debug.Log($"Se hizo clic en la zona: RIGHT");
-                break;
-            case Lane.Bottom:
-                Debug.Log($"Se hizo clic en la zona: BOTTOM");
-                break;
-            case Lane.Left:
-                Debug.Log($"Se hizo clic en la zona: LEFT");
-                break;
+            switch (lane)
+            {
+                case Lane.Top:
+                    RespawnAllie(0);
+                    Debug.Log($"Se hizo clic en la zona: TOP");
+                    break;
+                case Lane.Right:
+                    RespawnAllie(1);
+                    Debug.Log($"Se hizo clic en la zona: RIGHT");
+                    break;
+                case Lane.Bottom:
+                    RespawnAllie(2);
+                    Debug.Log($"Se hizo clic en la zona: BOTTOM");
+                    break;
+                case Lane.Left:
+                    RespawnAllie(3);
+                    Debug.Log($"Se hizo clic en la zona: LEFT");
+                    break;
+            }
         }
-        
     }
 
     private void OnMouseEnter()
@@ -48,10 +72,41 @@ public class RespawnAllies : MonoBehaviour
         SetCursor(defaultCursor);
     }
 
-    void SetCursor(Texture2D cursorTexture)
+    public void SetCursor(Texture2D cursorTexture)
     {
-        currentCursor = cursorTexture;
-        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+        if (GameController.instance.CheckInPanel())
+        {
+            currentCursor = cursorTexture;
+            Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+        }
+    }
+
+    void RespawnAllie(int indexRespawn)
+    {
+        switch (currentCharacter)
+        {
+            case 1:
+                if (GameController.instance.amountPj1 > 0)
+                {
+                    Instantiate(pj1Prefab, respawns[indexRespawn].position, respawns[indexRespawn].rotation);
+                    GameController.instance.amountPj1--;
+                }
+                break;
+            case 2:
+                if (GameController.instance.amountWolf > 0)
+                {
+                    Instantiate(wolfPrefab, respawns[indexRespawn].position, respawns[indexRespawn].rotation);
+                    GameController.instance.amountWolf--;
+                }      
+                break;
+            case 3:
+                if (GameController.instance.amountPj3 > 0)
+                {
+                    Instantiate(pj3Prefab, respawns[indexRespawn].position, respawns[indexRespawn].rotation);
+                    GameController.instance.amountPj3--;
+                }
+                break;
+        }
     }
 }
 public enum Lane
